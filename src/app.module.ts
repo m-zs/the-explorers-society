1c3286d3fbe3e7c943909353c92b4dcd208d688f
M. Zasada <mzasada93@gmail.com>
2025-02-08 17:18:46 +0100

    chore: add config paths, config e2e

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { DatabaseModule } from '@core/database/database.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
