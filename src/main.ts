cdcd8bf90af9c83ac8163c44a1cc855282289aff
M. Zasada <mzasada93@gmail.com>
2025-02-10 21:47:19 +0100

    feat: add open api setup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Logger as CoreLogger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { LogService } from '@core/logging/log.service';
+import { setupOpenApi } from '@core/open-api/setup-open-api';
 import { exceptionFactory } from '@core/validation/exception.factory';
 
 import { AppModule } from './app.module';
@@ -9,9 +10,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // logging
   const logger = app.get(LogService);
   CoreLogger.overrideLogger(logger);
 
+  // global validators
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -19,6 +22,9 @@ async function bootstrap() {
       exceptionFactory,
     }),
   );
+
+  // open API
+  setupOpenApi(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
