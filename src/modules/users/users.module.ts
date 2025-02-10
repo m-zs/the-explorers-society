ff35fbff8a823b03d84f1cdae55d0f02b8bc830d
M. Zasada <mzasada93@gmail.com>
2025-02-10 22:06:33 +0100

    fix: add missing password service provider

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { PasswordService } from '@core/services/password/password.service';
+
 import { UsersController } from './users.controller';
 import { UserRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UserRepository, UsersService],
+  providers: [PasswordService, UserRepository, UsersService],
 })
 export class UsersModule {}
