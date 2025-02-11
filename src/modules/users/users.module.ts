a046069c7ade2542ff04db5011945c3f52588f5e
M. Zasada <mzasada93@gmail.com>
2025-02-11 20:37:51 +0100

    feat: add users cli

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { PasswordService } from '@core/services/password/password.service';
 
@@ -7,7 +8,9 @@ import { UserRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [UsersController],
   providers: [PasswordService, UserRepository, UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
