683f4dae9ae5207d11fa9fa8b7831c7fb1a95554
M. Zasada <mzasada93@gmail.com>
2025-02-12 20:27:02 +0100

    feat: add roles service, controller

--- src/modules/roles/models/role.model.ts
+++ src/modules/roles/models/role.model.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { BaseModel } from '@core/database/models/base.model';
 import { UserModel } from '@modules/users/models/user.model';
 import { RoleType } from '@modules/users/role.enum';
@@ -5,8 +7,16 @@ import { RoleType } from '@modules/users/role.enum';
 export class RoleModel extends BaseModel {
   static tableName = 'roles';
 
+  @ApiProperty({ description: 'Unique identifier for the role', example: 1 })
   id!: number;
+
+  @ApiProperty({ description: 'Name of the role', example: 'Admin' })
   name!: string;
+
+  @ApiProperty({
+    description: 'Type of role',
+    example: 'Tenant',
+  })
   type!: RoleType;
 
   static relationMappings = {
