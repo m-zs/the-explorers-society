46fe0250b7278b17b06d8df9ca76b2b7cb8c1f4a
M. Zasada <mzasada93@gmail.com>
2025-02-12 21:14:18 +0100

    fix: role dto

--- src/modules/roles/dto/create-role.dto.ts
+++ src/modules/roles/dto/create-role.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 import { RoleType } from '@modules/users/role.enum';
 
@@ -7,11 +8,16 @@ export class CreateRoleDto {
     description: 'Name of the role',
     example: 'Admin',
   })
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({
     description: 'Type of role',
     example: RoleType.GLOBAL,
+    enum: RoleType,
   })
+  @IsEnum(RoleType)
+  @IsNotEmpty()
   type: RoleType;
 }
