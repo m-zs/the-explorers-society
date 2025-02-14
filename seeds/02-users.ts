e7da749577b4ba1f078bf7e78314287329b89013
M. Zasada <mzasada93@gmail.com>
2025-02-14 19:43:56 +0100

    feat: add users seed

---
+++ seeds/02-users.ts
@@ -0,0 +1,27 @@
+import 'tsconfig-paths/register';
+import { ConfigService } from '@nestjs/config';
+import { Knex } from 'knex';
+import { faker } from '@faker-js/faker';
+import { PasswordService } from '@core/services/password/password.service';
+
+export async function seed(knex: Knex): Promise<void> {
+  await knex('users').del();
+
+  const passwordService = new PasswordService(new ConfigService());
+
+  const users = await Promise.all(
+    Array.from({ length: 5 }, async () => {
+      const name = faker.string.uuid();
+      const password = await passwordService.hashPassword(name);
+      return {
+        name,
+        email: faker.internet.email(),
+        password,
+      };
+    }),
+  );
+
+  await knex('users').insert(users);
+
+  console.log('Tenants seeded successfully');
+}
