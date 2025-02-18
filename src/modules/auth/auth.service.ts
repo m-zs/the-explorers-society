0020e93dc3a539ac017309770f061fdc1ac61a67
M. Zasada <mzasada93@gmail.com>
2025-02-18 20:59:20 +0100

    refactor: adjust auth base

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,27 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Injectable()
 export class AuthService {
-  create(_createAuthDto: CreateAuthDto) {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async signIn(_signInDto: SignInDto) {
     return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, _updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
   }
 }
