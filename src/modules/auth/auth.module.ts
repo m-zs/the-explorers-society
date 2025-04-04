b5105f013b1fd8af40cc9f6249db171467b6f284
M. Zasada <mzasada93@gmail.com>
2025-04-04 21:45:49 +0200

    feat: extend auth service with password validation

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { PasswordService } from '@core/services/password/password.service';
 
 import { AuthController } from './auth.controller';
+import { AuthRepository } from './auth.repository';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthRepository, JwtService, PasswordService],
 })
 export class AuthModule {}
